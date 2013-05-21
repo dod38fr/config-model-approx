c78269c770639b959a7f3250988c6a9f5a71f109
Dominique Dumont <dod@debian.org>
2013-05-21 20:02:57 +0200

    Fix GPL denomination to keep Debian's lintian happy

--- lib/Config/Model/Approx.pm
+++ lib/Config/Model/Approx.pm
@@ -59,7 +59,7 @@ Dominique Dumont, (ddumont at cpan dot org)
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Lesser Public License for more details.
 
-   You should have received a copy of the GNU Lesser Public License
+   You should have received a copy of the GNU Lesser General Public License
    along with Config-Model; if not, write to the Free Software
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 
